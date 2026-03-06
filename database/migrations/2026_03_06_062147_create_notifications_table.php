0497199732e0fe1e8aaa7402e8d81dd3d47f05c6
ShahadBAuqu <s44510503@uqu.edu.sa>
2026-03-06 10:11:32 +0300

    add tournaments and Noutifications

---
+++ database/migrations/2026_03_06_062147_create_notifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+           // Primary Key and Fields for Notification
+        $table->id('notificationID');
+        $table->unsignedBigInteger('receiverID');
+        $table->unsignedBigInteger('userID');
+        $table->string('tournamentName')->nullable();
+        $table->unsignedBigInteger('tournamentCalendarID')->nullable();
+        $table->unsignedBigInteger('followID')->nullable();
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
