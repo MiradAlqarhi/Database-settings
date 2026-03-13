e4cfb6024c69af8b2108418dabdbbe887583cb01
AlzahraaAlyami <s445007348@uqu.edu.sa>
2026-03-13 23:28:23 +0300

    Add scouts and follows tables

---
+++ database/migrations/2026_03_09_070350_create_follows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->increments('FollowID');
+            $table->unsignedBigInteger('FollowerID');
+            $table->unsignedBigInteger('FollowingID');
+            $table->unsignedBigInteger('UserID');
+            $table->timestamp();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('follows');
+    }
+};
